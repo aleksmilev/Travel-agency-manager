2018dbced85466c0b0c5208e50eb31eb453d0b79
aleksmilev <aleksmilev1@gmail.com>
2023-10-05 18:38:19 +0300

    d

--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { EmptyPageComponent } from './empty-page/empty-page.component';
 
+import { AuthGuardAdmin } from '../auth/auth-admin.guard';
+import { AuthGuardUser } from '../auth/auth-user.guard';
+import { AuthGuardClient } from '../auth/auth-client.guard';
+
+const auth = true;
 
 const Routing: Routes = [
   {
-    path: '',
-    component: EmptyPageComponent
+    path: 'test',
+    component: EmptyPageComponent,
+    canActivate: [AuthGuardAdmin]
   },
-
-
-
   {
     path: '**',
     redirectTo: '',
