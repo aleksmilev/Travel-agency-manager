d0d2ccf8c7639538e7b9f408dc1c3dd796247067
aleksmilev <aleksmilev1@gmail.com>
2023-10-13 14:52:57 +0300

    1

--- src/app/pages/empty-page/empty-page.component.ts
+++ src/app/pages/empty-page/empty-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from 'src/app/modules/data.service';
+import { DataService } from 'src/app/services/data.service';
+import { EncryptionService } from 'src/app/services/encryption.service';
 
 @Component({
 	selector: 'app-empty-page',
@@ -8,9 +9,28 @@ import { DataService } from 'src/app/modules/data.service';
 })
 export class EmptyPageComponent implements OnInit {
 
-  	constructor(private http: DataService) {}
+	hashed_data: any = null;
+	normal_data: any = null;
+
+  	constructor(
+		private http: DataService,
+		private encryption: EncryptionService
+		) {}
 
 	ngOnInit(): void {
+		let data = {
+			'email': 'testmail@gmail.com',
+			'username': 'testuser',
+			'password': 'passw1234',
+			'role': 'anmin'
+		}
+
+		// this.hashed_data = this.hash.encrypt_data(data);
+		// localStorage.setItem("info", this.hashed_data);
+
+		// this.normal_data = this.hash.decrypt_data(this.hashed_data);
+
+
 		// this.http.Get('test').subscribe(data => {
 		// 	this.data = JSON.stringify(data);
 		// });
---
+++ src/app/services/encryption.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EncryptionService {
+    private encryptionKey: string = '2w7X9cR$#mP!zF5T@K&yH8eB';
+
+    encrypt(data: any): string {
+        data = JSON.stringify(data);
+
+        let result = '';
+
+        for (let i = 0; i < data.length; i++) {
+            const charCode = data.charCodeAt(i) ^ this.encryptionKey.charCodeAt(i % this.encryptionKey.length);
+            result += String.fromCharCode(charCode);
+        }
+
+        return btoa(result);
+    }
+
+    decrypt(encrypted_data: any): string {
+        encrypted_data = atob(encrypted_data);
+
+        let result = '';
+
+        for (let i = 0; i < encrypted_data.length; i++) {
+            const charCode = encrypted_data.charCodeAt(i) ^ this.encryptionKey.charCodeAt(i % this.encryptionKey.length);
+            result += String.fromCharCode(charCode);
+        }
+        
+        return JSON.parse(result);
+    }
+
+}
